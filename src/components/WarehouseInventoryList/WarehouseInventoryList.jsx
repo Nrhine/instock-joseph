b97e712d5c9261e72a28bf055c2a8dcf78668d05
sungchulkim <37005088+sungchulkim@users.noreply.github.com>
2024-09-04 16:52:07 -0700

    added the tags

--- src/components/WarehouseInventoryList/WarehouseInventoryList.jsx
+++ src/components/WarehouseInventoryList/WarehouseInventoryList.jsx
@@ -7,35 +7,40 @@ function WarehouseInventoryList() {
     return (
         <>
             <div className="inventory__container">
+            {/* {fetchedData.map((item) => ( key={item.id} */}
                 <div className="inventory__container-upper-mobile">
                     <div className="inventory__container-unit">
                         <h4 className="inventory__inventory-menu">Inventory Item</h4>
-                        <h3 className="inventory__inventory-item">Television</h3>
+                        <h3 className="inventory__inventory-item">Television &gt; </h3>
                     </div>
                     <div className="inventory__container-unit">
                         <h4 className="inventory__inventory-menu">Status</h4>
-                        <h3 className="inventory__inventory-item">IN STOCK</h3>
+                        <h3 className="inventory__inventory-status">IN STOCK</h3>
                     </div>
                 </div>
                 <div className="inventory__container-middle-mobile">
                     <div className="inventory__container-unit">
                         <h4 className="inventory__inventory-menu">Category</h4>
-                        <h3 className="inventory__inventory-item">Electronics</h3>
+                        <h3 className="inventory__inventory-category">Electronics</h3>
                     </div>
                     <div className="inventory__container-unit">
                         <h4 className="inventory__inventory-menu">QTY</h4>
-                        <h3 className="inventory__inventory-item">500</h3>
+                        <h3 className="inventory__inventory-quantity">500</h3>
                     </div>
                 </div>
                 <div className="inventory__container-bottom-mobile">
                     <div className="inventory__container-unit">
                         <h4 className="inventory__inventory-menu">Warehouse</h4>
-                        <h3 className="inventory__inventory-item">King West</h3>
+                        <h3 className="inventory__inventory-warehouse">King West</h3>
                     </div>
                 </div>
+
+                <span class="in-stock-tag">In Stock</span>
+                <span class="out-of-stock-tag">Out of Stock</span>
+
                 <span className="inventory__recycle-bin"><img src={recycleBin} alt="delete" /></span>
                 <span className="inventory__recycle-bin"><img src={pencil} alt="delete" /></span>
-
+            {/* ))} */}
             </div>
         </>
     )
