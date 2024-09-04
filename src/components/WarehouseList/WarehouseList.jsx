2abbaae8b6adeb32c84275f596f0831e8a53a227
Howard Huang <howard.huang3+git@gmail.com>
2024-09-04 17:34:38 -0400

    Build WarehouseList and add Warehouse Item

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,40 +1,42 @@
 import CTA from '../CTA/CTA'
 import FormField from '../FormField/FormField'
 import './WarehouseList.scss'
-
-import trash from '../../assets/Icons/delete_outline-24px.svg'
-import edit from '../../assets/Icons/edit-24px.svg'
+import WarehouseItem from '../WarehouseItem/WarehouseItem'
 
 function WarehouseList() {
+
+    const sampleData = {
+        "warehouse_name": "Manhattan",
+        "address": "503 Broadway",
+        "city": "New York",
+        "country": "USA",
+        "contact_name": "Parmin Aujla",
+        "contact_phone": "+1 (616) 123-1234",
+        "contact_email": "paujla@instock.com"
+    }
+
   return (
     <>
         <div className='warehouse-list'>
             <div className="warehouse-list__head">
                 <h1 className="warehouse-list__title">Warehouses</h1>
-                <FormField className="form-field--search warehouse-list__search" placeholder="Search..."/>
-                <CTA className="CTA--primary warehouse-list__add" text="+ Add New Warehouse"/>
+                <div className="warehouse-list__nav">
+                    <FormField className="form-field--search warehouse-list__search" placeholder="Search..."/>
+                    <CTA className="CTA--primary warehouse-list__add" text="+ Add New Warehouse"/>
+                </div>
             </div>
             <div className="warehouse-list__table">
-                <div className="warehouse-item">
-                    <div className="warehouse-item__body">
-                        <div className="warehouse-item__location">
-                            <p className="warehouse-item__label">WAREHOUSE</p>
-                            <p className="warehouse-item__copy warehouse-item__link">Manhattan</p>
-                            <p className="warehouse-item__label">ADDRESS</p>
-                            <p className="warehouse-item__copy">503 Broadway, New York, USA</p>
-                        </div>
-                        <div className="warehouse-item__contact">
-                            <p className="warehouse-item__label">CONTACT NAME</p>
-                            <p className="warehouse-item__copy">PARMIN AUJLA</p>
-                            <p className="warehouse-item__label">CONTACT INFORMATION</p>
-                            <p className="warehouse-item__copy">paujla@instock.com</p>
-                        </div>
+                <div className="warehouse-list__header-row">
+                    <div className="warehouse-list__table-headers">
+                        <h4 className="warehouse-list__table-header">WAREHOUSE</h4>
+                        <h4 className="warehouse-list__table-header">ADDRESS</h4>
+                        <h4 className="warehouse-list__table-header">CONTACT NAME</h4>
+                        <h4 className="warehouse-list__table-header">CONTACT INFORMATION</h4>
                     </div>
-                    <div className="warehouse-item__icons">
-                        <img src={trash} alt="delete icon" className="warehouse-item__icon" />
-                        <img src={edit} alt="delete icon" className="warehouse-item__icon" />
-                    </div>
+                    <h4 className="warehouse-list__action-header">ACTIONS</h4>
                 </div>
+                <WarehouseItem data={sampleData} />
+                <WarehouseItem data={sampleData} />
             </div>
 
         </div>
