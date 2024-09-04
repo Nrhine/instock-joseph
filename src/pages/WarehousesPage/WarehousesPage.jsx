e49ae38a8b3eef2dd946d6e6b60ed32e15b6ecee
Angelo <yapangeloterezo@gmail.com>
2024-09-04 12:38:48 -0700

    footer component fully responsive

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,10 +1,12 @@
 import "./WarehousesPage.scss";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const WarehousesPage = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
