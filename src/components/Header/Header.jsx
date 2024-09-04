a6a5002d897561c3491899c29b5a921d2f7f2796
Angelo <yapangeloterezo@gmail.com>
2024-09-04 10:49:16 -0700

    Header component fully responsive, added button-default styling in _mixins

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import "./Header.scss";
 import logo from "../../assets/Logo/Instock-Logo.svg";
+import Button from "../Button/Button";
 
 const Header = () => {
   return (
     <nav className="header">
       <img src={logo} alt="instock logo" className="header__logo" />
-      <ul className="header__list">
-        <li className="header">Warehouses</li>
-        <li className="header">Inventory</li>
-      </ul>
+      <div className="header__nav">
+        <Button
+          text="Warehouses"
+          className="button__header button__header--active"
+        />
+        <Button
+          text="Inventory"
+          className="button__header button__header--inactive"
+        />
+      </div>
     </nav>
   );
 };
