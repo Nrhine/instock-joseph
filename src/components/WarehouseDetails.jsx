581423f1e42ff73a87c46bf4f90e86f159edf2df
Nico Reincheld <nicoreincheld@gmail.com>
2024-09-04 14:31:48 -0400

    Commit before merging develop into feature branch

--- src/components/WarehouseDetails.jsx
+++ src/components/WarehouseDetails.jsx
@@ -1,11 +1,16 @@
 import './WarehouseDetails.scss';
-import back_arrow from '../assets/Icons/arrow';
+import back_arrow from '../assets/Icons/arrow_back-24px.svg';
+import edit from '../assets/Icons/edit-white-24px.svg';
 
 function WarehouseDetails() {
   return (
     <>
       <div className="warehouse__container">
-        <div className="warehouse__container-top"></div>
+        <div className="warehouse__container-top">
+          <img src={back_arrow} alt="go back" />
+          <h1>Washington</h1>
+          <img src={edit} alt="edit warehouse" className="warehouse" />
+        </div>
       </div>
     </>
   );
