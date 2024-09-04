74d56e4d2938b06ac08f7e45f04f65cd09ae581c
sungchulkim <37005088+sungchulkim@users.noreply.github.com>
2024-09-04 08:03:12 -0700

    J2J-3 Front-End Setup:React Router. Updated App.jsx with placeholder and added empty folder under components and pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import './App.scss';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {}
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<h1>WarehouseListPage</h1>} />
+                <Route path="/inventory" element={<h1>InventoryListPage</h1>} />
+            </Routes>
+        </Router>
+    );
+}
 
 export default App;
