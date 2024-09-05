37cd7afa42c08765e326d0eedd5c72d4f647d8f6
Angelo <yapangeloterezo@gmail.com>
2024-09-05 09:24:51 -0700

    DeleteWarehousePopup done but needs a few adjustments once used as a popup

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -2,6 +2,7 @@ import "./WarehousesPage.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import DeleteInventoryPopUp from "../../components/DeleteInventoryPopUp/DeleteInventoryPopUp";
+import DeleteWarehousePopUp from "../../components/DeleteWarehousePopup/DeleteWarehousePopup";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 
 const WarehousesPage = () => {
@@ -9,6 +10,7 @@ const WarehousesPage = () => {
     <>
       <Header />
       <DeleteInventoryPopUp />
+      <DeleteWarehousePopUp />
       <WarehouseList />
       {/* <Footer /> */}
     </>
