3bf9bef141795d1b69cc7e09fa2bf38f937c8413
Nico Reincheld <nicoreincheld@gmail.com>
2024-09-05 15:13:09 -0400

    Commiting to pull changes to develop branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.scss';
 import React from 'react';
-import WarehousesPage from './pages/WarehousesPage/WarehousesPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import WarehousesPage from './pages/WarehousesPage/WarehousesPage';
+import WarehouseDetailsPage from './pages/WarehouseDetialsPage/WarehouseDetailsPage';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<WarehousesPage />} />
-        <Route path="/inventory" element={<h1>InventoryListPage</h1>} />
+        <Route path="/:id" element={<WarehouseDetailsPage />} />
+        {/* // <Route path="/inventory" element={<h1>InventoryListPage</h1>} /> */}
       </Routes>
     </Router>
   );
---
+++ src/pages/WarehouseDetialsPage/WarehouseDetailsPage.jsx
@@ -0,0 +1,12 @@
+import './WarehouseDetailsPage.scss';
+import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails';
+
+function WarehouseDetailsPage() {
+  return (
+    <>
+      <WarehouseDetails />
+    </>
+  );
+}
+
+export default WarehouseDetailsPage;
