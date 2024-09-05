cc4b78e984b18ac279a0074e78a10f42ec0d38a7
Angelo <yapangeloterezo@gmail.com>
2024-09-05 08:29:37 -0700

    replaced button components with CTA component

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -2,12 +2,14 @@ import "./WarehousesPage.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import DeleteInventoryPopUp from "../../components/DeleteInventoryPopUp/DeleteInventoryPopUp";
+import WarehouseList from "../../components/WarehouseList/WarehouseList";
 
 const WarehousesPage = () => {
   return (
     <>
       <Header />
       <DeleteInventoryPopUp />
+      <WarehouseList />
       <Footer />
     </>
   );
