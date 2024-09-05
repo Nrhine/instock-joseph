43edb3f2991b5d9c610ec6e012d70cf4eafa9109
Angelo <yapangeloterezo@gmail.com>
2024-09-05 09:12:15 -0700

    DeleteInventoryPopUp done but needs a few adjustments once used as popup

--- src/components/DeleteInventoryPopUp/DeleteInventoryPopUp.jsx
+++ src/components/DeleteInventoryPopUp/DeleteInventoryPopUp.jsx
@@ -1,10 +1,11 @@
 import "./DeleteInventoryPopUp.scss";
 import close from "../../assets/Icons/close-24px.svg";
+import CTA from "../CTA/CTA";
 
 const DeleteInventoryPopUp = () => {
   return (
     <div className="delete-inventory-popup">
-      <img src={close} alt="close" />
+      <img src={close} alt="close" className="delete-inventory-popup__close" />
       <h1 className="delete-inventory-popup__title">
         Delete Television inventory item?
       </h1>
@@ -12,8 +13,9 @@ const DeleteInventoryPopUp = () => {
         Please confirm that you'd like to delete Television from the inventory
         list. You Won't be able to undo this action.
       </p>
-      <div className="delete-inventory-popup__button-container">
-        buttons here
+      <div className="delete-inventory-popup__buttons-container">
+        <CTA className="CTA--secondary" text="Cancel" />
+        <CTA className="CTA--delete" text="Delete" />
       </div>
     </div>
   );
--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -10,7 +10,7 @@ const WarehousesPage = () => {
       <Header />
       <DeleteInventoryPopUp />
       <WarehouseList />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
