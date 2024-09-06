5dbe425378ed9d4273b2c698acaafd3862c54e1e
Angelo <yapangeloterezo@gmail.com>
2024-09-05 18:55:55 -0700

    adjusted app.jsx to display warehouses page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.scss";
-//import HomePage from './pages/HomePage';
-//import WarehousesPage from './pages/WarehousesPage';
+import WarehousesPage from "./pages/WarehousesPage/WarehousesPage";
 import AddWarehousePage from "./pages/AddWarehousePage";
 import EditWarehousePage from "./pages/EditWarehousePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -9,11 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        {/*
-                <Route path="/" element={<WarehousesPage />} />
-                <Route path="/inventory" element={<h1>InventoryListPage</h1>} />
-                 */}
+        <Route path="/" element={<WarehousesPage />} />
+        <Route path="/inventory" element={<h1>InventoryListPage</h1>} />
         <Route path="/warehouse/add" element={<AddWarehousePage />} />
         <Route path="/warehouse/:id/edit" element={<EditWarehousePage />} />
       </Routes>
