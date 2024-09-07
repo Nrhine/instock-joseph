2b1dfca3632d8ed8d78bba3c6fcd9e878951653c
sungchulkim <37005088+sungchulkim@users.noreply.github.com>
2024-09-07 09:51:02 -0700

    Added the link to the inventory item

--- src/components/WarehouseInventoryList/WarehouseInventoryList.jsx
+++ src/components/WarehouseInventoryList/WarehouseInventoryList.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import recycleBin from '../../assets/Icons/delete_outline-24px.svg';
 import pencil from '../../assets/Icons/edit-24px.svg';
 import chevron from '../../assets/Icons/chevron_right-24px.svg';
+import DeleteInventoryPopUp from '../DeleteInventoryPopUp/DeleteInventoryPopUp';
 
 function WarehouseInventoryList({ data, name }) {
   return (
     <>
-      {/* <div className="outer"> */}
+
       <div className="inventory__container">
         <div className="inventory__hide-mobile">
           <h4 className="inventory__inventory-menu inventory__inventory-menu-item">Inventory Item</h4>
@@ -18,7 +19,7 @@ function WarehouseInventoryList({ data, name }) {
           <h4 className="inventory__inventory-menu">Actions</h4>
         </div>
 
-        
+
 
         <div className="inventory__outer">
           {data.map((item) => (
@@ -28,10 +29,12 @@ function WarehouseInventoryList({ data, name }) {
 
                   <div className="inventory__wrap">
                     <h4 className="inventory__inventory-menu inventory__mobile">Inventory Item</h4>
-                    <div className="inventory__item-chevron">
-                      <h3 className="inventory__inventory-item-name inventory__item">{item.item_name}</h3>
-                      <img className="inventory__chevron" src={chevron} alt="link icon" />
-                    </div>
+                    <Link to={`/inventory/${item.id}`}>
+                      <div className="inventory__item-chevron">
+                        <h3 className="inventory__inventory-item-name inventory__item">{item.item_name}</h3>
+                        <img className="inventory__chevron" src={chevron} alt="link icon" />
+                      </div>
+                    </Link>
                   </div>
 
                   <div className="inventory__wrap">
@@ -40,7 +43,7 @@ function WarehouseInventoryList({ data, name }) {
                   </div>
                 </div>
 
-                <div className="inventory__wrapper inventory__wrapper-bigger">    
+                <div className="inventory__wrapper inventory__wrapper-bigger">
                   <div className="inventory__wrap">
                     <h4 className="inventory__inventory-menu inventory__mobile">Status</h4>
                     <h4 className={`inventory__item ${item.status === 'In Stock' ? 'in-stock-tag' : 'out-of-stock-tag'}`}>{item.status}</h4>
@@ -64,20 +67,19 @@ function WarehouseInventoryList({ data, name }) {
 
               <h4 className="inventory__inventory-menu inventory__hide">Actions</h4>
               <div className="inventory__actions">
-                <span className="inventory__recycle-bin"><img src={recycleBin} alt="delete" /></span>
-                <Link to={`/edit-inventory/${item.id}`}>
-                  <span className="inventory__recycle-bin"><img src={pencil} alt="edit" /></span>
+                {/* <Link to={<DeleteInventoryPopUp data={item.id} />}> */}
+                  <span className="inventory__recycle-bin"><img src={recycleBin} alt="delete" /></span>
+                {/* </Link> */}
+                <Link to={`/inventory/${item.id}`}>
+                  <span className="inventory__edit"><img src={pencil} alt="edit" /></span>
                 </Link>
               </div>
-              
             </div>
-            
           ))}
-          
         </div>
       </div>
       <div className="inventory__separator"></div>
-      {/* </div> */}
+
     </>
   );
 }
