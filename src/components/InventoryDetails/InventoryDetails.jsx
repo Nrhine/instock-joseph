16cd2f4ac7a1b174752f1edd0519005a849be404
Howard Huang <howard.huang3+git@gmail.com>
2024-09-08 22:20:19 -0400

    Add api call to inventory details

--- src/components/InventoryDetails/InventoryDetails.jsx
+++ src/components/InventoryDetails/InventoryDetails.jsx
@@ -1,11 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import './InventoryDetails.scss'
 import back_arrow from '../../assets/Icons/arrow_back-24px.svg';
 import edit from '../../assets/Icons/edit-white-24px.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function InventoryDetails() {
 
-    let status = 'In Stock';
+    let itemId = useParams().id;
+    const apiUrl = `${import.meta.env.VITE_API_URL}`
+    const [inventoryData, setInventoryData] = useState([]);
+
+    const fetchItem = async () => {
+        try {
+            let response = await axios.get(
+                apiUrl + "/inventory"
+            )
+            console.log(response.data[itemId])
+            setInventoryData(response.data[itemId])
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchItem()
+    }, [])
 
   return (
     <>
@@ -14,7 +34,7 @@ function InventoryDetails() {
                 <Link to={'/'}>
                     <img src={back_arrow} alt="go back" />
                 </Link>
-                <h1 className="inventory-detail__title">Television</h1>
+                <h1 className="inventory-detail__title">{inventoryData.item_name}</h1>
                 <Link to={'/'}>
                     <div className="inventory-detail__edit-container">
                         <img src={edit} alt="Edit Inventory Icon" className="inventory-detail__edit" />
@@ -27,29 +47,29 @@ function InventoryDetails() {
                 <div className="inventory-detail__left">
                     <div className="inventory-detail__row">
                         <h4 className="inventory-detail__label">ITEM DESCRIPTION:</h4>
-                        <p className="p2 inventory-detail__copy">This 50", 4K LED TV provides a crystal-clear picture and vivid colors.</p>
+                        <p className="p2 inventory-detail__copy">{inventoryData.description}</p>
                     </div>
                     <div className="inventory-detail__row">
                         <h4 className="inventory-detail__label">CATEGORY:</h4>
-                        <p className="p2 inventory-detail__copy">Electronics</p>
+                        <p className="p2 inventory-detail__copy">{inventoryData.category}</p>
                     </div>
                 </div>
                 <div className="inventory-detail__right">
                     <div className="inventory-detail__row--split">
                         <div className="inventory-detail__cell">
                             <h4 className="inventory-detail__label">STATUS:</h4>
-                            <div className={`inventory-detail__tag ${status === 'In Stock' ? 'inventory-detail__tag--in-stock' : 'inventory-detail__tag--out-of-stock'}`}>
-                                <p className={`p2 inventory-detail__copy ${status === 'In Stock' ? 'inventory-detail__stock--in' : 'inventory-detail__stock--out'}`}>In Stock</p>
+                            <div className={`inventory-detail__tag ${inventoryData.status === 'In Stock' ? 'inventory-detail__tag--in-stock' : 'inventory-detail__tag--out-of-stock'}`}>
+                                <p className={`p2 inventory-detail__copy ${inventoryData.status === 'In Stock' ? 'inventory-detail__stock--in' : 'inventory-detail__stock--out'}`}>{inventoryData.status}</p>
                             </div>
                         </div>
                         <div className="inventory-detail__cell">
                             <h4 className="inventory-detail__label">QUANTITY:</h4>
-                            <p className="p2 inventory-detail__copy">500</p>
+                            <p className="p2 inventory-detail__copy">{inventoryData.quantity}</p>
                         </div>
                     </div>
                     <div className="inventory-detail__row">
                         <h4 className="inventory-detail__label">WAREHOUSE:</h4>
-                        <p className="p2 inventory-detail__copy">Manhattan</p>
+                        <p className="p2 inventory-detail__copy">{inventoryData.warehouse_name}</p>
                     </div>
                 </div>
                 
